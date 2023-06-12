ad283b7161291961c626ea740ff0cfa3561f8a80
Ata <atakanozalan@gmail.com>
2023-06-12 14:04:16 +0300

    printing the textField.text

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -57,12 +57,22 @@ class ToDoListViewController: UITableViewController {
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        var textField = UITextField()
+        
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
             //what will happen once the user clicks the Add Item button on our UIAlert
-            print("Success!")
+            print(textField.text)
         }
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+            
+        }
+        
         alert.addAction(action)
         
         present(alert, animated: true,completion: nil)
