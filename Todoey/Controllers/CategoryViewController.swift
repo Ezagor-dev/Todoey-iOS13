ff21a3afee5dbe807e5dbed20943a81079969c95
Ata <atakanozalan@gmail.com>
2023-06-19 20:57:57 +0300

    Creating category

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Ezagor on 19.06.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController{
+    
+}
