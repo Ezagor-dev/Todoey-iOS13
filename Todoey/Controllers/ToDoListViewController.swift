6079315a5185b2570d85dab704ab781d896d8341
Ata <atakanozalan@gmail.com>
2023-07-01 04:27:00 +0300

    fixing ToDoListViewController pin button bug

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -16,6 +16,10 @@ class ToDoListViewController: SwipeTableViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     var todoItems: Results<Item>?
+    var pinnedItems: Results<Item>?
+    var unpinnedItems: Results<Item>?
+    
+    
     let realm = try! Realm()
     
     var selectedCategory : Category? {
@@ -50,12 +54,12 @@ class ToDoListViewController: SwipeTableViewController {
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let selectedBackgroundView = UIView()
-            selectedBackgroundView.backgroundColor = .black
-            cell.selectedBackgroundView = selectedBackgroundView
+        selectedBackgroundView.backgroundColor = .black
+        cell.selectedBackgroundView = selectedBackgroundView
         if cell.contentView.layer.cornerRadius != 10.0 {
-                // Set the corner radius of the cell's content view
-                cell.contentView.layer.cornerRadius = 10.0
-            }
+            // Set the corner radius of the cell's content view
+            cell.contentView.layer.cornerRadius = 10.0
+        }
         let verticalPadding: CGFloat = 10.0
         let maskLayer = CALayer()
         maskLayer.backgroundColor = UIColor.white.cgColor
@@ -69,7 +73,7 @@ class ToDoListViewController: SwipeTableViewController {
         cell.contentView.layer.mask = maskLayer
         cell.contentView.layer.masksToBounds = true
     }
-
+    
     
     override func viewWillAppear(_ animated: Bool) {
         if let colourHex = selectedCategory?.colour{
@@ -90,83 +94,94 @@ class ToDoListViewController: SwipeTableViewController {
     //MARK: - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todoItems?.count ?? 1
+        return todoItems?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let minHeight: CGFloat = 80
         let defaultHeight: CGFloat = 44
-
+        
         if let item = todoItems?[indexPath.row] {
             let text = item.title
             let labelWidth = tableView.bounds.width - 16 // Adjust the padding as needed
             let labelFont = UIFont.systemFont(ofSize: 17)
             let labelInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10) // Adjust the insets as needed
-
+            
             let label = UILabel()
             label.font = labelFont
             label.text = text
             label.numberOfLines = 0
             label.frame.size = CGSize(width: labelWidth, height: .greatestFiniteMagnitude)
             label.sizeToFit()
-
+            
             let lines = ceil(label.frame.height / label.font.lineHeight)
             let lineSpacing: CGFloat = 4 // Adjust the line spacing as needed
             let cellHeight = max(minHeight, (label.font.lineHeight * lines) + (lineSpacing * (lines - 1)) + labelInsets.top + labelInsets.bottom)
-
+            
             return cellHeight
         }
-
+        
         return defaultHeight
     }
-
-
-
-
-
-
-
+    
 
     override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return 44 // Set an initial estimated height for better performance
     }
     
+    
+    
+    
+    
     //cellForRowAt
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
         // Add the creation date label to the cell
-            let creationDateLabel = UILabel()
-            creationDateLabel.translatesAutoresizingMaskIntoConstraints = false
-            cell.contentView.addSubview(creationDateLabel)
-            
-            // Set the creation date label constraints
-            creationDateLabel.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 20).isActive = true
-            creationDateLabel.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -20).isActive = true
-            creationDateLabel.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: -10).isActive = true
-
+        let creationDateLabel = UILabel()
+        creationDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        cell.contentView.addSubview(creationDateLabel)
+        
+        // Set the creation date label constraints
+        creationDateLabel.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 20).isActive = true
+        creationDateLabel.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -20).isActive = true
+        creationDateLabel.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: -10).isActive = true
+        
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
+            
+//            if item.isPinned {
+//                // Apply styling for pinned item
+//                cell.backgroundColor = UIColor.red
+//                cell.textLabel?.textColor = .black
+//                
+//            } else {
+//                // Apply styling for unpinned item
+//                cell.backgroundColor = UIColor.white
+//                cell.textLabel?.textColor = .black
+//            }
+            
             // Display the creation date in the creation date label
             let dateFormatter = DateFormatter()
             dateFormatter.locale = Locale(identifier: "en_US_POSIX")
             dateFormatter.dateFormat = "MMM d, yyyy 'at' h:mm a"
             let creationDate = item.dateCreated
             let creationDateString = dateFormatter.string(from: creationDate)
-
+            
             creationDateLabel.text = "Created on \(creationDateString)"
-                    
-                    // Customize the appearance of the creation date label
-                    creationDateLabel.font = UIFont.italicSystemFont(ofSize: 12)
-                    creationDateLabel.textColor = UIColor.gray
+            
+            // Customize the appearance of the creation date label
+            creationDateLabel.font = UIFont.italicSystemFont(ofSize: 12)
+            creationDateLabel.textColor = UIColor.gray
             cell.accessoryType = item.done ? .checkmark : .none
-
+            
             if let categoryColor = UIColor(hexString: selectedCategory?.colour ?? "") {
                 let contrastColor = calculateContrastColor(forColor: categoryColor)
                 cell.contentView.layer.borderWidth = 1.0
                 cell.contentView.layer.borderColor = categoryColor.cgColor
                 cell.contentView.layer.backgroundColor = categoryColor.cgColor
                 cell.textLabel?.textColor = indexPath.row == 0 && isColorDark(categoryColor) ? .black : contrastColor
-                cell.tintColor = contrastColor
+                cell.tintColor = .white
                 
                 // Update label's properties
                 cell.textLabel?.numberOfLines = 0
@@ -186,12 +201,12 @@ class ToDoListViewController: SwipeTableViewController {
                 cell.contentView.layer.borderColor = UIColor.lightGray.cgColor
                 cell.textLabel?.textColor = .black
             }
-
+            
             let startColor = UIColor(hexString: selectedCategory?.colour ?? "") ?? UIColor(red: 152/255, green: 238/255, blue: 204/255, alpha: 1.0)
             let maxItems = CGFloat(todoItems?.count ?? 1)
             let percentage = CGFloat(indexPath.row) / maxItems
             let endColor = startColor.darken(byPercentage: 0.3 + (0.4 * percentage))
-
+            
             if item.isPinned {
                 cell.backgroundColor = endColor
                 cell.textLabel?.textColor = calculateContrastColor(forColor: endColor)
@@ -209,37 +224,30 @@ class ToDoListViewController: SwipeTableViewController {
             cell.textLabel?.numberOfLines = 1 // Show only one line for the placeholder text
             creationDateLabel.text = ""
         }
-
+        
         // Set the background color of the cell
         cell.backgroundColor = .black
-
+        
         return cell
     }
 
-
+    
+    
     func isColorDark(_ color: UIColor) -> Bool {
         guard let components = color.cgColor.components else {
             return false
         }
-
+        
         let red = components[0]
         let green = components[1]
         let blue = components[2]
-
+        
         let threshold: CGFloat = 0.5
         let luminance = (red * 0.299) + (green * 0.587) + (blue * 0.114)
-
+        
         return luminance < threshold
     }
-
-
-
-
-
-
-
-
-
+    
 
     
     //MARK: - TableView Delegate Methods
@@ -271,7 +279,7 @@ class ToDoListViewController: SwipeTableViewController {
     override func editModel(at indexPath: IndexPath) {
         let alert = UIAlertController(title: "Edit Item", message: "", preferredStyle: .alert)
         var textField = UITextField()
-
+        
         let action = UIAlertAction(title: "Update", style: .default) { (action) in
             if let newItemName = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
                !newItemName.isEmpty, let item = self.todoItems?[indexPath.row] {
@@ -291,21 +299,21 @@ class ToDoListViewController: SwipeTableViewController {
                 self.present(errorAlert, animated: true, completion: nil)
             }
         }
-
+        
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
+        
         alert.addTextField { (field) in
             textField = field
             textField.text = self.todoItems?[indexPath.row].title
             textField.autocapitalizationType = .sentences
         }
-
+        
         alert.addAction(action)
         alert.addAction(cancelAction)
-
+        
         present(alert, animated: true, completion: nil)
     }
-
+    
     
     //MARK: - Delete Data From Swipe
     
@@ -344,36 +352,44 @@ class ToDoListViewController: SwipeTableViewController {
         pinAction.image = UIImage(systemName: "pin.fill")
         
         editAction.backgroundColor = .blue
-            pinAction.backgroundColor = .gray
+        pinAction.backgroundColor = .gray
         
         return [deleteAction, editAction, pinAction]
     }
-
     
     func pinItem(at indexPath: IndexPath) {
-        if let item = todoItems?[indexPath.row] {
+        if let selectedCategory = selectedCategory, let item = todoItems?[indexPath.row] {
             do {
                 try realm.write {
                     item.isPinned = !item.isPinned
-                    
-                    let allItems = realm.objects(Item.self).sorted(byKeyPath: "isPinned", ascending: false)
-                    let pinnedItems = allItems.filter("isPinned == true")
-                    let unpinnedItems = allItems.filter("isPinned == false")
-                    
-                    let concatenatedItems = Array(pinnedItems) + Array(unpinnedItems)
-                    todoItems = realm.objects(Item.self).filter("FALSEPREDICATE") // Empty filter to clear the previous Results object
-                    todoItems = realm.objects(Item.self).sorted(byKeyPath: "isPinned", ascending: false)
-                    
-                    tableView.reloadData()
                 }
+                
+                // Retrieve all items belonging to the selected category
+                let allItems = selectedCategory.items.sorted(byKeyPath: "title", ascending: true)
+                
+                // Sort the items based on the pin status and title
+                let pinnedItems = allItems.filter("isPinned == true").sorted(byKeyPath: "title", ascending: true)
+                let unpinnedItems = allItems.filter("isPinned == false").sorted(byKeyPath: "title", ascending: true)
+                let updatedItems = Array(pinnedItems) + Array(unpinnedItems)
+                
+                // Replace the items in the selected category with the updated sorted items
+                try realm.write {
+                    selectedCategory.items.removeAll()
+                    selectedCategory.items.append(objectsIn: updatedItems)
+                }
+                
+                tableView.reloadData()
             } catch {
-                print("Error updating item pin status, \(error)")
+                print("Error updating item pin status: \(error)")
             }
         }
     }
 
 
+
     
+    
+ 
     //MARK: - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -383,59 +399,58 @@ class ToDoListViewController: SwipeTableViewController {
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         
         let addAction = UIAlertAction(title: "Add Item", style: .default) { (_) in
-                    let itemName = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
-
-                    if let name = itemName, !name.isEmpty, let currentCategory = self.selectedCategory {
-                        do {
-                            try self.realm.write {
-                                let newItem = Item()
-                                newItem.title = name.capitalized
-                                newItem.dateCreated = Date()
-                                currentCategory.items.append(newItem)
-                            }
-                        } catch {
-                            print("Error saving new items, \(error)")
-                        }
-                    }else{
-                        let errorAlert = UIAlertController(title: "Error", message: "Item name cannot be empty.", preferredStyle: .alert)
-                        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                        errorAlert.addAction(okAction)
-                        self.present(errorAlert, animated: true, completion: nil)
+            let itemName = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            if let name = itemName, !name.isEmpty, let currentCategory = self.selectedCategory {
+                do {
+                    try self.realm.write {
+                        let newItem = Item()
+                        newItem.title = name.capitalized
+                        newItem.dateCreated = Date()
+                        currentCategory.items.append(newItem)
                     }
-                    
-                    self.tableView.reloadData()
+                } catch {
+                    print("Error saving new items, \(error)")
                 }
-
-                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
-                alert.addAction(addAction)
-                alert.addAction(cancelAction)
-
-                alert.addTextField { (alertTextField) in
-                    textField = alertTextField
-                    textField.placeholder = "Create new item"
-                    textField.autocapitalizationType = .sentences
-                }
-
-                present(alert, animated: true, completion: nil)
+            }else{
+                let errorAlert = UIAlertController(title: "Error", message: "Item name cannot be empty.", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                errorAlert.addAction(okAction)
+                self.present(errorAlert, animated: true, completion: nil)
             }
-
+            
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        
+        alert.addTextField { (alertTextField) in
+            textField = alertTextField
+            textField.placeholder = "Create new item"
+            textField.autocapitalizationType = .sentences
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     
     //MARK - Model Manupulation Methods
     
     
     
     func loadItems() {
-            if let category = selectedCategory {
-                // Sort the items based on pin status and then by title
-                todoItems = category.items.sorted(by: [
-                    SortDescriptor(keyPath: "isPinned", ascending: false),
-                    SortDescriptor(keyPath: "title", ascending: true)
-                ])
-            }
-            
-            tableView.reloadData()
+        if let category = selectedCategory {
+            // Sort the items based on the pin status and then by title
+            todoItems = category.items.sorted(by: [
+                SortDescriptor(keyPath: "isPinned", ascending: false),
+                SortDescriptor(keyPath: "title", ascending: true)
+            ])
         }
+    }
+    
     
 }
 
@@ -450,9 +465,6 @@ extension ToDoListViewController: UISearchBarDelegate {
         tableView.reloadData()
     }
     
-    
-    
-    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
             loadItems()
@@ -462,12 +474,7 @@ extension ToDoListViewController: UISearchBarDelegate {
             }
             
         }
-  
+        
     }
-    
-    
-    
-    
-    
 }
 
