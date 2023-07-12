cb44af1d08106ddce8e82873098d3853266aa445
Ata <atakanozalan@gmail.com>
2023-07-12 19:18:27 +0300

    changing launch screen

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
             print("Realm file URL: \(Realm.Configuration.defaultConfiguration.fileURL)")
 
             let userDefaults = UserDefaults.standard
